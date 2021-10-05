9b1dd3411e5edac1de2abfd265db392833b33473
shindeanant <shindeanant1997@gmail.com>
2021-10-05 15:16:14 +0530

    ADD:Checking equality of two lines based on to points

--- LineComparision.java
+++ LineComparision.java
@@ -3,7 +3,6 @@ package com.LineComparision;
 import java.util.Scanner;
 
 public class LineComparision {
-
 	static Scanner scan = new Scanner(System.in);
 
 	public void calculateLength() {
@@ -16,10 +15,33 @@ public class LineComparision {
 		System.out.println("length is:" + len1);
 	}
 
+	public void equalityOfTwoLines() {
+		System.out.println("Enter the values:");
+		double x1 = scan.nextDouble();
+		double x2 = scan.nextDouble();
+		double y1 = scan.nextDouble();
+		double y2 = scan.nextDouble();
+		double a1 = scan.nextDouble();
+		double a2 = scan.nextDouble();
+		double b1 = scan.nextDouble();
+		double b2 = scan.nextDouble();
+
+		double len1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		double len2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+		System.out.println("length is:" + len1);
+		System.out.println("length is:" + len2);
+		if (len1 == len2) {
+			System.out.println("len1s are equal");
+		} else {
+			System.out.println("both lines are not equal");
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
 		LineComparision object = new LineComparision();
 		object.calculateLength();
+		object.equalityOfTwoLines();
 	}
 
 }
