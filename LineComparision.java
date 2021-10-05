81d35cf294272e6c865602eac0ea6fb557f50f60
shindeanant <shindeanant1997@gmail.com>
2021-10-05 14:33:02 +0530

    ADD: Welcome message

---
+++ LineComparision.java
@@ -0,0 +1,9 @@
+package com.LineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+
+}
